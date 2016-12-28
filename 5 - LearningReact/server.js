3e64f351ffc9d56555585ee13eb94ec934cb941a
FlorinSenoner <florin.senoner@gmail.com>
2016-12-29 00:42:25 +0100

    remove old files

--- 5 - LearningReact/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-
-// create app
-var app = express()
-app.use(express.static('public'))
-
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000')
-})
