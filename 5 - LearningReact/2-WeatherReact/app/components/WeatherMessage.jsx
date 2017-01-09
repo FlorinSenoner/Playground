bb504fc853e2f24334c0cf09e8a1cd62bfd8360c
FlorinSenoner <florin.senoner@gmail.com>
2017-01-09 17:42:26 +0100

    remove unneeded files

--- 5 - LearningReact/2-WeatherReact/app/components/WeatherMessage.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react')
-
-var WeatherMessage = ({location, temp}) => { // es6 destructorin -> define location and temp with {} instead of props
-  return (
-    <p>It is {temp}°C in {location}</p>
-  )
-}
-
-module.exports = WeatherMessage
